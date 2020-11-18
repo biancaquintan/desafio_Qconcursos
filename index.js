0cc33f967b132a2746c5f4c6722f03853ccca56b
Bianca <bsquintan@gmail.com>
2020-11-18 17:23:30 -0300

    adjustments

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var request = new XMLHttpRequest();
 
 request.onload = getData;
-request.open('get', 'https://api.github.com/users/biancaquintan', true)
+request.open('get', 'https://api.github.com/users/biancaquintan')
 request.send()
 
 function getData() {
@@ -9,6 +9,7 @@ function getData() {
 
     let avatar = document.getElementById('avatar')
     let profile_link = document.getElementById('profile')
+    let nick = document.getElementById('nick')
     let repos = document.getElementById('repos')
     let followers = document.getElementById('followers')
     let following = document.getElementById('following')
@@ -16,6 +17,7 @@ function getData() {
     avatar.setAttribute("src", `${responseObj.avatar_url}`);    
     profile_link.setAttribute("href", `${responseObj.html_url}`);
 
+    nick.innerHTML = `${responseObj.login}`
     repos.innerHTML = `<em>${responseObj.public_repos}</em>`
     followers.innerHTML = `<em>${responseObj.followers}</em>`
     following.innerHTML = `<em>${responseObj.following}</em>`
